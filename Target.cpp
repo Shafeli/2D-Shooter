d1f8e5c4cfe1e42e6f6800712142dd9fe247798a
Shafeli <59191017+Shafeli@users.noreply.github.com>
2021-12-07 19:13:51 -0800

    12/7/21
    target prototype movement started -

--- Target.cpp
+++ Target.cpp
@@ -1,5 +1,5 @@
 #include "Target.h"
-
+#include <iostream>
 Target::Target(GameDataRef data)
     :m_data(data)
 {
@@ -15,5 +15,36 @@ void Target::Draw()
 
 void Target::Update(float dt)
 {
+  
+        // Check left side of the window
+    if (m_sprite.getGlobalBounds().left <= 0.f)
+        m_sprite.move(m_sprite.getGlobalBounds().width / 9 ,0.f);
 
+    //check right side of the window 
+    if (m_sprite.getGlobalBounds().left + m_sprite.getGlobalBounds().width >= m_data->window.getSize().x)
+        m_sprite.move(-m_sprite.getGlobalBounds().width / 9, 0.f);
+
+    if (m_left == false)
+    {
+        // move to left then tell window edge then move right tell window edge repeat
+        if (!m_sprite.getGlobalBounds().left <= 0.f)
+        {
+            m_sprite.move(-m_movementSpeed, 0.f);
+        }
+        if (m_sprite.getGlobalBounds().left <= 0.f)
+        {
+            std::cout << "has touched left\n";
+            m_left = true;
+            return;
+        }
+    }
+    if (m_left == true)
+    {
+        m_sprite.move(+m_movementSpeed, 0.f);
+        if (m_sprite.getGlobalBounds().left + m_sprite.getGlobalBounds().width >= m_data->window.getSize().x)
+        {
+            std::cout << "has touched right\n";
+            m_left = false;
+        }
+    }
 }
--- Target.h
+++ Target.h
@@ -6,7 +6,8 @@ class Target
 {
     GameDataRef m_data;
     sf::Sprite m_sprite;
-
+    float m_movementSpeed = 5.f;
+    bool m_left = false;
   
 public:
     Target(GameDataRef data);
